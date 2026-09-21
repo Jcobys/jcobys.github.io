ecebb24904d360df58fc34bdb453ec0ea3134c78
zl <180986697@qq.com>
2026-09-21 11:44:37 +0800

    Use lowercase novelshift directory and URLs

---
+++ novelshift/_next/static/chunks/app/layout-9720e65cad6b4457.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1082:()=>{},8771:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,1082,23))}},e=>{e.O(0,[509,587,938,358],()=>e(e.s=8771)),_N_E=e.O()}]);
